abd2c7e0289c2b6a0c147bfe405cc3c84dec0aa0
cwooni <129937268+cwooni@users.noreply.github.com>
2023-11-21 08:14:09 +0000

    Add files via upload

--- OCR.py
+++ OCR.py
@@ -61,7 +61,7 @@ def response(response):
        
 if __name__ == "__main__":
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"openocr-394310-95d8b763df38.json"
-    client = OpenAI(api_key='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')
+    client = OpenAI()
     output_text=main()
     messages=[]
     # user_content=input("user : ")
